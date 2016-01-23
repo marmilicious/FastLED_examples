cb4a56b695c73aebb27aab13fef3de1626a13f10
Marc Miller <marmilicious@users.noreply.github.com>
2016-01-23 11:42:31 -0800

    Update test_three_moving_pixels_v3.ino
    simplified code and added EVERY_N for doing the delay.

--- test_three_moving_pixels_v3.ino
+++ test_three_moving_pixels_v3.ino
@@ -1,27 +1,31 @@
 //***************************************************************
 // Three moving pixels example
+//     Uses modulo, %, to make pixel position "loop" around and
+//     stay in valid pixel range.
+// Marc Miller, Feb 2015.  Updated Jan 2016.
 //***************************************************************
 
-#include "FastLED.h"
-#define LED_TYPE NEOPIXEL  // ***Update to your strip type.  NEOPIXEL, APA102, LPD8806, etc.
-#define DATA_PIN 6  // ***Remember to set this to your data pin
-//#define CLOCK_PIN 13  // ***Remember to set this to your clock pin
-#define NUM_LEDS 12  // ***Remember to set this to your strip length
-//#define COLOR_ORDER BGR
-#define MASTER_BRIGHTNESS 100  // Master brightness range is 0-255
-CRGB leds[NUM_LEDS];
-int16_t positionWhite = 4;  // Set initial start position of White pixel
-int16_t positionRed = 0;  // Set initial start position of Red pixel
-int16_t positionBlue = 8;  // Set initial start position of Blue pixel
 
-uint16_t holdTime = 1000;  // Milliseconds to hold position before advancing
-int8_t delta = -1;  // 1 or -1.  Sets forward or backwards direction.
+#include "FastLED.h"
+#define LED_TYPE NEOPIXEL  // Strip type: NEOPIXEL, APA102, LPD8806, etc.
+#define DATA_PIN 6
+//#define CLOCK_PIN 13
+#define NUM_LEDS 12
+//#define COLOR_ORDER BGR
+#define MASTER_BRIGHTNESS 100  // Master brightness (Range is 0-255)
+CRGB leds[NUM_LEDS];
+
+int16_t positionRed = 0;    // Set initial start position of Red pixel
+int16_t positionWhite = 4;  // Set initial start position of White pixel
+int16_t positionBlue = 8;   // Set initial start position of Blue pixel
+int8_t delta = 1;           // Using a negative value will move pixels backwards.
+uint16_t holdTime = 1000;   // Milliseconds to hold position before advancing
 
 
 //---------------------------------------------------------------
 void setup() {
-  delay(1000);  // Startup delay
-  //Serial.begin(57600);  // Allows serial monitor output (check baud rate)
+  Serial.begin(115200);  // Allows serial monitor output (check your baud rate)
+  delay(3000);  // Startup delay
   //FastLED.addLeds<LED_TYPE,DATA_PIN,CLOCK_PIN>,COLOR_ORDER,(leds, NUM_LEDS);  // ***For strips using Clock.
   FastLED.addLeds<LED_TYPE,DATA_PIN>(leds, NUM_LEDS);  // ***For Clock-less strips.
   FastLED.setBrightness(MASTER_BRIGHTNESS);
@@ -31,28 +35,33 @@ void setup() {
 //---------------------------------------------------------------
 void loop() {  // START MAIN LOOP
 
-  // Set position and color of pixels
-  leds[(positionWhite + delta + NUM_LEDS) % NUM_LEDS] = CRGB::White;
-  leds[(positionRed + delta + NUM_LEDS) % NUM_LEDS] = CRGB::Red;
-  leds[(positionBlue + delta + NUM_LEDS) % NUM_LEDS] = CRGB::Blue;
+  EVERY_N_MILLISECONDS(holdTime) {
 
-  // Show the pixels
-  // NOTE:  Using FastLED.delay will automatically also "show" the pixels,
-  // therefore no need for a FastLED.show imediately followed by FastLED.delay
-  //FastLED.show();
-  FastLED.delay(holdTime);  // Delay for a bit.
- 
-  // Set pixels back to Black for the next loop around.
-  leds[(positionWhite + delta + NUM_LEDS) % NUM_LEDS] = CRGB::Black;
-  leds[(positionRed + delta + NUM_LEDS) % NUM_LEDS] = CRGB::Black;
-  leds[(positionBlue + delta + NUM_LEDS) % NUM_LEDS] = CRGB::Black;
+    // Set pixel color
+    leds[positionRed] = CRGB::Red;
+    leds[positionWhite] = CRGB::White;
+    leds[positionBlue] = CRGB::Blue;
+  
+    // Show the pixels
+    FastLED.show();
+    //delay(holdTime);  // Delay for a bit.
+   
+    // Set pixels back to Black for the next loop around.
+    leds[positionRed] = CRGB::Black;
+    leds[positionWhite] = CRGB::Black;
+    leds[positionBlue] = CRGB::Black;
+  
+    // Set new position, moving (forward or backward) by delta.
+    // NUM_LEDS is added to the position before doing the modulo
+    // to cover cases where delta is a negative value.
+    positionRed = (positionRed + delta + NUM_LEDS) % NUM_LEDS;
+    positionWhite = (positionWhite + delta + NUM_LEDS) % NUM_LEDS;
+    positionBlue = (positionBlue + delta + NUM_LEDS) % NUM_LEDS;
 
-  // Advance position based on delta, and rollover if needed.
-  positionWhite = ((positionWhite + delta + NUM_LEDS) % NUM_LEDS);
-  positionRed = ((positionRed + delta + NUM_LEDS) % NUM_LEDS);
-  positionBlue = ((positionBlue + delta + NUM_LEDS) % NUM_LEDS);
+    // Print out position values to see what's happening.
+    Serial.print("pos R = "); Serial.print(positionRed);
+    Serial.print("\t  pos W = "); Serial.print(positionWhite);
+    Serial.print("\t  pos B = "); Serial.println(positionBlue);
+  }
 
-
-  //Serial.print("position = "); Serial.println(positionWhite);
-
-}  // END MAIN LOOP
+} // END MAIN LOOP
