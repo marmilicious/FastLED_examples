68a36da0c8ffbbed50186411b2b15b9f2014ec7a
Marc Miller <marmilicious@users.noreply.github.com>
2017-07-29 00:22:55 -0700

    Create print_HSV_values.ino

---
+++ print_HSV_values.ino
@@ -0,0 +1,51 @@
+//***************************************************************
+//  Print out HSV values to serial monitor example...
+//
+//  Marc Miller, July 2017
+//***************************************************************
+
+#include "FastLED.h"
+#define DATA_PIN    11
+#define CLOCK_PIN   13
+#define LED_TYPE    LPD8806
+#define COLOR_ORDER GBR
+#define NUM_LEDS    32
+
+#define BRIGHTNESS  50
+CRGB leds[NUM_LEDS];
+CHSV hsv[NUM_LEDS];
+
+
+//---------------------------------------------------------------
+void setup() {
+  Serial.begin(115200);  // Allows serial monitor output (check baud rate)
+  delay(1500); // startup delay for recovery
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,DATA_PIN,CLOCK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+}
+
+
+//---------------------------------------------------------------
+void loop() {
+  EVERY_N_SECONDS( 5 ) {
+
+    for (uint8_t i=0; i < NUM_LEDS; i++) {
+      hsv[i].h = random8();
+      hsv[i].s = random8(200,256);
+      hsv[i].v = random8(64,85);
+      leds[i] = CHSV(hsv[i].h, hsv[i].s, hsv[i].v);
+
+      Serial.print("HSV["); Serial.print(i);
+      Serial.print("]: "); Serial.print(hsv[i].h);
+      Serial.print(" "); Serial.print(hsv[i].s);
+      Serial.print(" "); Serial.println(hsv[i].v);
+    }
+    
+    FastLED.show();  
+
+  }//end_EVERY_N
+
+}//end_main_loop
+
