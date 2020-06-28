391ebbfa7f10544cf71c3d525e2a4acfee4924ec
Marc Miller <marmilicious@users.noreply.github.com>
2020-06-27 22:00:35 -0700

    Add files via upload

---
+++ every_n_timer_variable_2.ino
@@ -0,0 +1,64 @@
+// EVERY_N variable timer example
+// Turns all pixels on for a short time then fades them out
+// to black.  Holds black for a short time and then repeats
+// On/Fade/Off pattern.  
+//
+// A new hue is displayed each time.
+//
+//
+// Marc Miller,  June 2020
+//---------------------------------------------------------------
+
+#include "FastLED.h"
+#define LED_TYPE WS2812B
+#define DATA_PIN 11
+#define NUM_LEDS 12
+#define COLOR_ORDER GRB
+#define BRIGHTNESS 255
+CRGB leds[NUM_LEDS];
+
+static uint8_t gHue;
+static boolean P = 0;
+
+
+//---------------------------------------------------------------
+void setup() { 
+  Serial.begin(115200);
+  delay(1500);  // startup delay
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+      .setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(BRIGHTNESS);
+  Serial.println("Setup done. \n");
+}
+
+//---------------------------------------------------------------
+
+
+void loop() {
+  
+  EVERY_N_MILLISECONDS_I( timingObj, 1) {
+    // This initally defaults to 1 millisecond, but then will
+    // imediately use the timingObj.setPeriod times below.
+    // You can name "timingObj" anything you want.
+
+    P = !P;  // toggle P between 0 and 1
+    
+    if (P) {
+      timingObj.setPeriod(1200);  // time to display solid before fading
+      fill_solid(leds, NUM_LEDS, CHSV(gHue, 255, 255));
+      FastLED.show();
+      Serial.print("fill_solid, hue: "); Serial.println(gHue);
+    } else {
+      timingObj.setPeriod(1200);
+      Serial.println("Fading out now...\n");
+      gHue = gHue + 42;  // set a new hue
+    }
+  }
+
+  if (P == 0) {
+    fadeToBlackBy(leds, NUM_LEDS, 2);  // fade out
+    FastLED.show();
+  }
+
+}  // end main loop
+
