5e46e331f3fc11d7e5c234479605f208adc11ca4
Marc Miller <marmilicious@users.noreply.github.com>
2023-03-31 12:04:47 -0700

    Update base_with_overlay.ino

--- base_with_overlay.ino
+++ base_with_overlay.ino
@@ -4,12 +4,14 @@
 // The overlay is toggled On/Off with a push button, but this could
 // be modified to triggered by anything else as needed.
 //
-// Line 86 can be uncommented if you only want the overlay pattern
+// Line 88 can be uncommented if you only want the overlay pattern
 // to run a single time affter it has been triggered.
 //
 // Marc Miller, Feb 2018
 //   Updated Jan 2020 - for JC_button library updates
+//   Updated March 2023 - use gHue instead of white and tweak fading
 //***************************************************************
+
 
 #include "FastLED.h"
 #define DATA_PIN    11
@@ -25,13 +27,14 @@ CRGB leds[NUM_LEDS];      //what is sent to pixels and displayed
 
 bool toggleButton = 0;    //toggled On/Off with each button press
 
+uint8_t gHue;             //slowly rotating hue color
 
 //---------------------------------------------------------------
 // This example uses JChristensen's Button Library from:
 //   https://github.com/JChristensen/JC_Button
 #include "JC_Button.h"       //include button library
 const uint8_t buttonPin = 4;  //set digital pin connected to momentary push button
-Button myButton(buttonPin, 50, true, true);  //declare the button  (pin, dbTime, puEnable, invert)
+Button myButton(buttonPin, 100, true, true);  //declare the button  (pin, dbTime, puEnable, invert)
 
 
 //---------------------------------------------------------------
@@ -53,6 +56,8 @@ void loop()
 {
   triggerCheck();     //check the overlay trigger (a push button in this example)
 
+  EVERY_N_MILLISECONDS(30) { gHue++; }  //slowly cycle through the rainbow
+
   basePattern();      //update the base display
   overlayPattern();   //update the overlay display (as needed)
   combinePatterns();  //combine the base and overlay before displaying
@@ -64,8 +69,6 @@ void loop()
 
 //---------------------------------------------------------------
 void basePattern() {  //NOTE: we are drawing to the "base" CRGB here
-  static uint8_t gHue = 0; //slowly rotating hue color
-  EVERY_N_MILLISECONDS( 30 ) { gHue++; }  //slowly cycle through the rainbow
   fill_rainbow( base, NUM_LEDS, gHue, 255/NUM_LEDS/2);
   fadeToBlackBy( base, NUM_LEDS, 220);  //dim the rainbow down
 }//end_basePattern
@@ -73,23 +76,26 @@ void basePattern() {  //NOTE: we are drawing to the "base" CRGB here
 
 //---------------------------------------------------------------
 void overlayPattern() {  //NOTE: we are drawing to the "overlay" CRGB here
-  if (toggleButton) {  //overlay is currently "On"
+  if (toggleButton) {  //if True then overlay is currently "On"
     EVERY_N_MILLISECONDS(60) {
       static uint8_t pos = 0;
-      overlay[pos] = overlay[NUM_LEDS-1-pos] = CRGB::White;
-      fadeToBlackBy( overlay, NUM_LEDS, 120);  //creates fading tail effect
+      overlay[pos] = overlay[NUM_LEDS-1-pos] = CHSV(gHue+128,180,255);
       pos++;
       if (pos == NUM_LEDS/2) {  //check against desired range
         pos = 0;  //reset for next round
-        // Uncomment this next line to have the overlay pattern only run a
-        // single time.  Otherwise the button toggles the overlay addition on/off.
+          // Uncomment this next line to have the overlay pattern only run a
+          // single time. Otherwise the button toggles the overlay addition on/off.
         //toggleButton = !toggleButton;  
       }
     }
 
+    EVERY_N_MILLISECONDS(15) {
+      fadeToBlackBy( overlay, NUM_LEDS, 30);  //creates fading tail effect
+    }
+
   } else {  //overlay is currently "Off"
-    EVERY_N_MILLISECONDS(30) {
-      fadeToBlackBy( overlay, NUM_LEDS, 50);  //fadeout overlay pattern
+    EVERY_N_MILLISECONDS(15) {
+      fadeToBlackBy( overlay, NUM_LEDS, 4);  //fadeout overlay pattern to zero
     }
   }
   
@@ -111,6 +117,11 @@ void combinePatterns() {
 // brightness levels.
 //
 // New = 255 - scale8( 255 - base, 255 - overlay);
+//
+//
+// FastLED's blend could also be used, but the ratio would need to
+// be animated in some fashion.
+// leds[i] = blend( base[i], overlay[i], ratio );  // [ratio is 0-255]
 //
 
 
