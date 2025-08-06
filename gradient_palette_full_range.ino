0c17455fba98d1b94827a6707fb5f494e6f6ec3c
Marc Miller <marmilicious@users.noreply.github.com>
2025-08-06 02:08:50 -0700

    Update gradient_palette_full_range.ino
    Updated to not use delay(). Also made it more interesting by animating the pixels coming on and fading everything off. Tweaked the custom palette too.

--- gradient_palette_full_range.ino
+++ gradient_palette_full_range.ino
@@ -19,7 +19,16 @@
 // For this example I decided to Not have the display wrap back
 // around, so I'm specifically using a palette index of 0-240.
 //
+// ***FastLED update in May 2023*** ColorFromPalette also now has
+// the blend type: LINEARBLEND_NOWRAP
+// This should be similar and not blend the end colors back into
+// the start colors, and you could still index all the way to 255.
+//
 // Marc Miller, Oct 2018
+//              Aug 2025 - Updated to not use delay(). Also made it
+//                         more interesting by animating the pixels
+//                         coming on and fading everything off.
+//                         Tweaked the custom palette too.
 //***************************************************************
 
 #include "FastLED.h"
@@ -30,6 +39,10 @@
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 #define BRIGHTNESS  128
+
+bool SHOW_COLORS = true;
+uint16_t pixelNumber;  // used to keep track of pixel position
+uint16_t count;
 
 
 //---------------------------------------------------------------
@@ -44,13 +57,13 @@ void setup() {
 // Create custom gradient palette
 DEFINE_GRADIENT_PALETTE( example_gp ) {
   0,    0,0,255,
-  5,    0,0,255,
- 80,    0,255,0,
-130,    0,0,255, 
-135,    0,0,255, 
-205,    255,0,255,
-238,    255,0,0,  // I'm making this my last color choice
-255,    255,0,0 };  // And then just duplicating it here
+  3,    0,40,200,
+ 50,    0,255,0,
+115,    30,50,120, 
+120,    80,50,255, 
+190,    255,0,255,
+238,    220,190,130,  // I'm making this my last color choice
+255,    220,190,130 };  // And then just duplicating it here
 
 CRGBPalette16 myPal = example_gp;  
 
@@ -58,13 +71,35 @@ CRGBPalette16 myPal = example_gp;
 //---------------------------------------------------------------
 void loop () {
 
-  for (uint8_t i=0; i<NUM_LEDS; i++) {
-    uint8_t paletteIndex = map(i,0,NUM_LEDS-1,0,240);  //index is now based on pixel number
-    leds[i] = ColorFromPalette( myPal, paletteIndex, BRIGHTNESS, LINEARBLEND);
+  EVERY_N_SECONDS(6) {  //do this every 6 seconds
+    SHOW_COLORS = true;  //time to run the animation again!
+    FastLED.clear();  //set all the pixels back to black
   }
-  
+
+
+  if (SHOW_COLORS == true) {
+    EVERY_N_MILLISECONDS(50) {
+      uint8_t paletteIndex = map(pixelNumber,0,NUM_LEDS-1,0,240);  //palette index is now based on pixel number
+      leds[pixelNumber] = ColorFromPalette( myPal, paletteIndex, BRIGHTNESS, LINEARBLEND);
+      pixelNumber++;  //go to next pixel
+      if (pixelNumber == NUM_LEDS) {
+        SHOW_COLORS = false;  //stop indexing up through the pixels
+        pixelNumber = 0;  //reset pixel count for next time
+        count = 0;  //reset count
+      }
+    }
+  } else {  //ie. SHOW_COLORS is false
+    EVERY_N_MILLISECONDS(10) {
+      if (count >= 300) {  //don't start fading for 10x300 = 3000 milliseconds
+        fadeToBlackBy( leds, NUM_LEDS, 5);  //fade all pixels a bit
+      }
+      count++;
+    }
+  }
+
   FastLED.show();
-  FastLED.delay(1000);
+
 
 }//end_main_loop
+
 
